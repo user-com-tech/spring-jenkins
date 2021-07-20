4eecd335422c3381af18cd176a4615f28ef0c0ad
Vivek Kumar Mishra <vivek.mishra1@comviva.com>
2021-07-20 12:04:34 +0530

    initial commit

---
+++ src/main/java/com/tech/jenkins/demo/SpringWithJenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.tech.jenkins.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringWithJenkinsApplication.class);
+	
+	public void init() {
+		logger.info("Application Started...");
+	}
+	public static void main(String[] args) {
+		logger.info("Application Running...");
+		SpringApplication.run(SpringWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/jenkins/demo/SpringWithJenkinsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.tech.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithJenkinsApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(SpringWithJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test is executing...");
+		
+	}
+
+}
