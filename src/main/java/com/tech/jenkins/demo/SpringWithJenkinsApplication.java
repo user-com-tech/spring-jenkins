15a19d254e8a5433cfaea556b61b466173b453df
Vivek Kumar Mishra <vivek.mishra1@comviva.com>
2021-07-20 15:42:04 +0530

    altered logger info

--- src/main/java/com/tech/jenkins/demo/SpringWithJenkinsApplication.java
+++ src/main/java/com/tech/jenkins/demo/SpringWithJenkinsApplication.java
@@ -15,7 +15,6 @@ public class SpringWithJenkinsApplication {
 	}
 	public static void main(String[] args) {
 		logger.info("Application Running...");
-		logger.info("Adding some extra statements...");
 		SpringApplication.run(SpringWithJenkinsApplication.class, args);
 	}
 
